d2b3d2f2afdf4396a7007613c61cd96b5d467528
Drew Winstel <drewbrew@users.noreply.github.com>
2018-08-29 21:12:38 -0500

    views: add initial viewsets

--- vetclinic/views.py
+++ vetclinic/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework.views import ModelViewSet
 
-# Create your views here.
+from . import models
+from . import serializers
+
+
+class ClientViewSet(ModelViewSet):
+    serializer_class = serializers.ClientSerializer
+    queryset = models.Client.objects.all()
+
+
+class SpeciesViewSet(ModelViewSet):
+    serializer_class = serializers.SpeciesSerializer
+    queryset = models.Species.objects.prefetch_related('technicians')
+
+
+class BreedViewSetWithWritablePK(ModelViewSet):
+    serializer_class = serializers.BreedSerializerWithWritablePK
+    queryset = models.Breed.objects.select_related('species')
+
+
+class BreedViewSetWithWritableNestedField(ModelViewSet):
+    serializer_class = serializers.BreedSerializerWithWritableSerializer
+    queryset = models.Breed.objects.select_related('species')
+
+
+class BreedViewSetWithSeparateIDField(ModelViewSet):
+    serializer_class = serializers.BreedSerializerWithSeparateWritablePK
+    queryset = models.Breed.objects.select_related('species')
