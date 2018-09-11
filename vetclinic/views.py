67c7ed931145f9f785ace6365a76ab72d804c282
Drew Winstel <drewbrew@users.noreply.github.com>
2018-09-10 20:39:08 -0500

    Add view for animal demonstrating get_queryset()

--- vetclinic/views.py
+++ vetclinic/views.py
@@ -27,3 +27,20 @@ class BreedViewSetWithWritableNestedField(ModelViewSet):
 class BreedViewSetWithSeparateIDField(ModelViewSet):
     serializer_class = serializers.BreedSerializerWithSeparateWritablePK
     queryset = models.Breed.objects.select_related('species')
+
+
+class AnimalViewSet(ModelViewSet):
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.action in ['retrieve', 'update', 'partial']:
+            # we're dealing with a single instance. Select the extra fields
+            # present in the detail serializer
+            return queryset.select_related('breed')
+        return queryset
+
+    # This is slightly counterintuitive, but you want to give the viewset the
+    # *DETAIL* serializer. The serializer will take care of subbing in the
+    # list serializer.
+    serializer_class = serializers.AnimalDetailSerializer
+    queryset = models.Animal.objects.select_related('species', 'client')
